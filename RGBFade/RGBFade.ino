e394fb23d811d62204857e7237e63108604244aa
Max Gonzih <gonzih@gmail.com>
2019-11-15 13:13:38 -0500

    Add RGB fade thing

---
+++ RGBFade/RGBFade.ino
@@ -0,0 +1,42 @@
+int tmp1 = 0;
+int tmp2 = 0;
+
+int prevLed = 9;
+int currLed = 10;
+int nextLed = 11;
+
+int limit = 200;
+int threshold = 50;
+int dl = 50;
+
+// fade amount
+int fa = 5;
+
+void setup() {
+  pinMode(prevLed, OUTPUT);
+  pinMode(currLed, OUTPUT);
+  pinMode(nextLed, OUTPUT);
+}
+
+void loop() {
+  for (int i = limit; i >= threshold; i-=fa) {
+    analogWrite(prevLed, i);
+    delay(dl);
+  }
+
+  for (int i = 0; i <= limit; i+=fa) {
+    analogWrite(currLed, i);
+    delay(dl);
+  }
+
+  for (int i = threshold; i >= 0; i-=fa) {
+    analogWrite(prevLed, i);
+    delay(dl);
+  }
+
+  tmp1 = currLed;
+  currLed = nextLed;
+  tmp2 = prevLed;
+  prevLed = tmp1;
+  nextLed = tmp2;
+}
