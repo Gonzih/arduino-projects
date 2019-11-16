6accc199eca210b360f2129cbdb66065a0932f1f
Max Gonzih <gonzih@gmail.com>
2019-11-16 10:47:26 -0500

    Oh i get it now!

--- RGBFade/RGBFade.ino
+++ RGBFade/RGBFade.ino
@@ -1,11 +1,11 @@
 int tmp = 0;
 
-int prevLed = 11;
+int prevLed = 9;
 int currLed = 10;
-int nextLed = 9;
+int nextLed = 11;
 
 int limit = 255;
-int dl = 100;
+int dl = 200;
 
 // fade amount
 int fa = 1;
@@ -17,8 +17,19 @@ void setup() {
   pinMode(nextLed, OUTPUT);
 }
 
+void log() {
+  Serial.print("prevLed: ");
+  Serial.println(prevLed);
+  Serial.print("currLed: ");
+  Serial.println(currLed);
+  Serial.print("nextLed: ");
+  Serial.println(nextLed);
+  Serial.println("=============");
+}
+
 void loop() {
-  for (int i = 0; i <= limit; i+=fa) {
+  /* log(); */
+  for (int i = limit; i >= 0; i-=fa) {
     analogWrite(prevLed, limit-i);
     analogWrite(currLed, i);
     delay(dl);
