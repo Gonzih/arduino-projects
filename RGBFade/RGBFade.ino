713683d7fd97fcf30bbe67390950cc4a8dfc1a1b
Max Gonzih <gonzih@gmail.com>
2019-11-15 19:57:53 -0500

    Fixes

--- RGBFade/RGBFade.ino
+++ RGBFade/RGBFade.ino
@@ -1,12 +1,10 @@
-int tmp1 = 0;
-int tmp2 = 0;
+int tmp = 0;
 
 int prevLed = 9;
 int currLed = 10;
 int nextLed = 11;
 
-int limit = 160;
-int threshold = 50;
+int limit = 255;
 int dl = 50;
 
 // fade amount
@@ -19,24 +17,15 @@ void setup() {
 }
 
 void loop() {
-  for (int i = limit; i >= threshold; i-=fa) {
-    analogWrite(prevLed, i);
-    delay(dl);
-  }
-
-  for (int i = 0; i <= limit; i+=fa) {
+  for (int i = 0; i >= limit; i+=fa) {
+    analogWrite(prevLed, limit-i);
     analogWrite(currLed, i);
     delay(dl);
   }
 
-  for (int i = threshold; i >= 0; i-=fa) {
-    analogWrite(prevLed, i);
-    delay(dl);
-  }
+  tmp = prevLed;
 
-  tmp1 = currLed;
+  prevLed = currLed;
   currLed = nextLed;
-  tmp2 = prevLed;
-  prevLed = tmp1;
-  nextLed = tmp2;
+  nextLed = tmp;
 }
